19a106502cc1084b40bb1d8e34dc70fdd3162023
Matt Thompson <matthew.thompson@nasa.gov>
2023-04-27 05:34:38 -0700

    Changes to port to Cray

---
+++ cmake/Cray.cmake
@@ -0,0 +1,12 @@
+# Compiler specific flags for Cray Fortran compiler
+
+set(no_optimize "-O0 -g")
+#set(check_all "-eC -eD -ec -en -eI -ea -g -G0")
+#set(traceback "-fbacktrace")
+set(cpp "-eF -eZ")
+
+
+set(CMAKE_Fortran_FLAGS_DEBUG  "${cpp} ${no_optimize} ${check_all} ${traceback}")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3 ${cpp}")
+#set(CMAKE_Fortran_FLAGS "-g ${cpp} -ffree-line-length-none")
+#set(CMAKE_Fortran_FLAGS "${cpp}")
